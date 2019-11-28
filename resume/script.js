19314a8539e2736a1602031896d2c1f54f7b2254
xiaowei_glb <xiaowei_glb@tom.com>
2019-11-28 10:57:44 +0800

    Site updated: 2019-11-28 10:57:40

--- resume/script.js
+++ resume/script.js
@@ -1,4 +1,9 @@
 $(function() {
+    // 年龄
+    let birthdayStr = $('#age').attr('data-birthday');
+    let age = getAge(birthdayStr);
+    $('#age').text(age + ' 岁');
+
     // 技能熟练度
     $('#skill-proficiency td').each(function(i) {
         let skillProficiency = $(this).attr('data-skill-proficiency');
@@ -16,4 +21,46 @@ $(function() {
         str = i + 1 + '、' + str;
         $(this).text(str);
     });
+
+    // 根据出生日期算出年龄
+    function getAge(strBirthday) {
+        var returnAge;
+        var strBirthdayArr = strBirthday.split("-");
+        var birthYear = strBirthdayArr[0];
+        var birthMonth = strBirthdayArr[1];
+        var birthDay = strBirthdayArr[2];
+
+        d = new Date();
+        var nowYear = d.getFullYear();
+        var nowMonth = d.getMonth() + 1;
+        var nowDay = d.getDate();
+
+        if (nowYear == birthYear) {
+            returnAge = 0; //同年 则为0岁
+        } else {
+            var ageDiff = nowYear - birthYear; //年之差
+            if (ageDiff > 0) {
+                if (nowMonth == birthMonth) {
+                    var dayDiff = nowDay - birthDay; //日之差
+                    if (dayDiff < 0) {
+                        returnAge = ageDiff - 1;
+                    } else {
+                        returnAge = ageDiff;
+                    }
+                } else {
+                    var monthDiff = nowMonth - birthMonth; //月之差
+                    if (monthDiff < 0) {
+                        returnAge = ageDiff - 1;
+                    } else {
+                        returnAge = ageDiff;
+                    }
+                }
+            } else {
+                returnAge = -1; //返回-1 表示出生日期输入错误 晚于今天
+            }
+        }
+
+        return returnAge; //返回周岁年龄
+
+    }
 });
